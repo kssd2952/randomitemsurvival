c4ef016e833500af293bed2027f86ce301f3bff7
choon52 <kssd2952@gmail.com>
2025-07-27 22:02:30 +0900

    debug

--- src/main/kotlin/io/github/kssd2952/event/RISEvent.kt
+++ src/main/kotlin/io/github/kssd2952/event/RISEvent.kt
@@ -1,23 +1,26 @@
 package io.github.kssd2952.event
 
 import io.github.kssd2952.Main
+import io.papermc.paper.event.player.PlayerPickItemEvent
 import net.kyori.adventure.text.Component
 import net.kyori.adventure.text.format.NamedTextColor
 import org.bukkit.Bukkit
 import org.bukkit.GameMode
+import org.bukkit.entity.EntityType
+import org.bukkit.entity.Player
 import org.bukkit.event.EventHandler
 import org.bukkit.event.Listener
+import org.bukkit.event.entity.EntityPickupItemEvent
 import org.bukkit.event.player.PlayerItemConsumeEvent
 
 class RISEvent : Listener {
     @EventHandler
-    fun onPlayerItemConsume(event: PlayerItemConsumeEvent) {
-        if (!Main.started) {
-            return
-        }
+    fun e(event: EntityPickupItemEvent) {
+        if (event.entity !is Player) return
+        if (!Main.started) return
 
-        val player = event.player
-        val item = event.item
+        val player = event.entity as Player
+        val item = event.item.itemStack
 
         if (item.type == Main.itemList[player.name]) {
             player.gameMode = GameMode.SPECTATOR
